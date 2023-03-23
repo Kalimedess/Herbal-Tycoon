172ac5f6c39c73ff3aceaa2776f8b77101cd16c1
Kalimedess <kalimedesss@gmail.com>
2023-03-23 14:13:45 +0100

    tsda

--- Assets/Scripts/BuyingPots.cs
+++ Assets/Scripts/BuyingPots.cs
@@ -1,26 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BuyingPots : MonoBehaviour
 {
     public GameObject pot;
     private Game gameManager;
-    [SerializeField] private Transform buttonPosition;
+    public GameObject buyButton;
+
+    private bool isButtonNull(GameObject Button) => Button.IsUnityNull();
+    private bool isPotNull(GameObject pot) => pot.IsUnityNull();
     private void Start()
     {
+        this.enabled = false;
         gameManager = GetComponent<Game>();
     }
-    public void buy(int requiredPoints)
+    private void OnEnable()
     {
-        if (gameManager.money > requiredPoints)
+        if (!isButtonNull(buyButton))
         {
-            Instantiate(pot,buttonPosition);
-            gameManager.money-=requiredPoints;
-        }
-        else
-        {
-            return;
+            if (!isPotNull(pot))
+            {
+                if (gameManager.money >= 500)
+                {
+                    Instantiate(pot, buyButton.transform);
+                    gameManager.money -= 500;
+                    gameManager.updateMoney();
+                    Destroy(buyButton);
+                }
+                else
+                {
+                    this.enabled = false;
+                    return;
+                }
+            }
         }
     }
 }
