1f622193374aecbac19162ec60f0622b5b13928f
PS-TS <115213337+PS-TS@users.noreply.github.com>
2023-04-20 14:30:05 +0200

    poprawiony wygląd inventory+czesciowedzialanie

---
+++ Assets/Scripts/inventory/Slot.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class Slot : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
+{
+    Image Target;
+    public Color32 BasicColor;
+    public Color32 OnEnterColor;
+    
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log("Click");
+    }
+    
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Target.color = OnEnterColor;
+    }
+    
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Target.color = BasicColor;
+    }
+
+    void Start()
+    {
+        Target = GetComponent<Image>();
+        Target.color = BasicColor;
+    }
+}
